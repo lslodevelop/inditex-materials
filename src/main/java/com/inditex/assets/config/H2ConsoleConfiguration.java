6b8842e883b92cc9250ff982f31e05f025d54c19
Luis López <lopezolmos.luis@gmail.com>
2025-10-28 12:29:42 +0100

    [REFACTOR] Getting rid of PostgreSQL since DB is simple and does not store files. H2 is enough and this way the project can be fully run locally without docker.

---
+++ src/main/java/com/inditex/assets/config/H2ConsoleConfiguration.java
@@ -0,0 +1,30 @@
+package com.inditex.assets.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+
+@Configuration
+@Profile("local")
+public class H2ConsoleConfiguration {
+
+    private Server webServer;
+    private Server tcpServer;
+
+    private final String WEB_PORT = "8082";
+    private final String TCP_PORT = "9092";
+
+    @EventListener(org.springframework.context.event.ContextRefreshedEvent.class)
+    public void start() throws java.sql.SQLException {
+        this.webServer = org.h2.tools.Server.createWebServer("-webPort", WEB_PORT, "-tcpAllowOthers").start();
+        this.tcpServer = org.h2.tools.Server.createTcpServer("-tcpPort", TCP_PORT, "-tcpAllowOthers").start();
+    }
+
+    @EventListener(org.springframework.context.event.ContextClosedEvent.class)
+    public void stop() {
+        this.tcpServer.stop();
+        this.webServer.stop();
+    }
+
+}
--- src/main/java/com/inditex/assets/config/LiquibaseDataSourceConfig.java
+++ src/main/java/com/inditex/assets/config/LiquibaseDataSourceConfig.java
@@ -1,34 +1,33 @@
 package com.inditex.assets.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class LiquibaseDataSourceConfig {
 
-    @Value("${spring.r2dbc.url}")
-    private String r2dbcUrl;
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
 
-    @Value("${spring.r2dbc.username}")
+    @Value("${spring.datasource.username}")
     private String username;
 
-    @Value("${spring.r2dbc.password}")
+    @Value("${spring.datasource.password}")
     private String password;
 
     @Bean
     @LiquibaseDataSource
     public DataSource liquibaseDataSource() {
-        String jdbcUrl = r2dbcUrl.replace("r2dbc:", "jdbc:");
         return DataSourceBuilder.create()
                 .url(jdbcUrl)
                 .username(username)
                 .password(password)
-                .driverClassName("org.postgresql.Driver")
+                .driverClassName("org.h2.Driver")
                 .build();
     }
 }
